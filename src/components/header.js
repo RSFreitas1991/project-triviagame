e109d0b93e2e397633de11f6bb9d7830757c795f
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-05 20:30:28 -0300

    Iniciando o requisito

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h1>
+        Tela do jogo
+      </h1>
+    );
+  }
+}
+
+export default Header;
