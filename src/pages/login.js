d1f30c5baeea407283d79915edddc7b2038ae512
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-05 16:18:24 -0300

    Requisito 1 acho que foi

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,20 +9,73 @@ class Login extends React.Component {
     this.state = {
       email: '',
       playerName: '',
-    /*       emailValidation: '',
-      nameValidation: '',
-      submitButton: '', */
+      emailValidation: false,
+      nameValidation: false,
+      submitButton: true,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.nameValidation = this.nameValidation.bind(this);
   }
 
   handleChange({ target }) {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => this.formValidation());
+  }
+
+  nameValidation() {
+    const { playerName } = this.state;
+    const playerLength = 1;
+    if (playerName.length >= playerLength) {
+      this.setState({
+        nameValidation: true,
+      }, () => this.submitButtonChange());
+    } else {
+      this.setState({
+        nameValidation: false,
+      }, () => this.submitButtonChange());
+    }
+  }
+
+  emailValidation() {
+    const { email } = this.state;
+    const emailState = email.match(/@/g);
+    const secondHalf = email.match(/@(.*)/);
+    const thirdHalf = email.match(/\.(.*)/);
+    if (emailState !== null
+      && emailState.length === 1
+      && secondHalf[1].length > 0
+      && thirdHalf !== null
+      && thirdHalf[1].length > 0) {
+      this.setState({
+        emailValidation: true,
+      }, () => this.submitButtonChange());
+    } else {
+      this.setState({
+        emailValidation: false,
+      }, () => this.submitButtonChange());
+    }
+  }
+
+  submitButtonChange() {
+    const { emailValidation, nameValidation } = this.state;
+    if (emailValidation === true && nameValidation === true) {
+      this.setState({
+        submitButton: false,
+      });
+    } else {
+      this.setState({
+        submitButton: true,
+      });
+    }
+  }
+
+  formValidation() {
+    this.emailValidation();
+    this.nameValidation();
   }
 
   render() {
-    const { email, playerName } = this.state;
+    const { email, playerName, submitButton } = this.state;
     return (
       <div className="main">
         <fieldset className="fieldset">
@@ -30,7 +83,7 @@ class Login extends React.Component {
           <input
             type="email"
             name="email"
-            data-testid="input-player-email"
+            data-testid="input-gravatar-email"
             onChange={ this.handleChange }
             value={ email }
             required
@@ -45,8 +98,9 @@ class Login extends React.Component {
           />
           <button
             type="button"
+            data-testid="btn-play"
             onClick={ () => console.log('cliquei') }
-            disabled={ false }
+            disabled={ submitButton }
           >
             Play
           </button>
