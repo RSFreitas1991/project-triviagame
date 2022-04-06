460257285d4600265c83076de66646bc79796bbc
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-05 22:08:47 -0300

    Requisito 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,6 @@ export function fetchToken() {
   return async (dispatch) => {
     const request = await fetch('https://opentdb.com/api_token.php?command=request');
     const response = await request.json();
-    console.log(response.token);
     dispatch(getToken(response.token));
   };
 }
