caa695a9e7e850e3fb0496eaafe011758b2ceb66
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-07 15:36:51 -0300

    Redux implementado

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { newAction } from '../actions';
+import { CHANGE_BUTTON_STATE } from '../reducers/questionsReducer';
 
 class Timer extends Component {
   constructor() {
@@ -23,6 +26,7 @@ class Timer extends Component {
 
   removeSeconds() {
     const { seconds } = this.state;
+    const { isAnswerButtonDisabled } = this.props;
     const lessSecond = seconds - 1;
     if (seconds !== 0) {
       this.setState({
@@ -30,6 +34,7 @@ class Timer extends Component {
       });
     } else {
       clearInterval(this.intervalID);
+      isAnswerButtonDisabled(true, CHANGE_BUTTON_STATE);
     }
   }
 
@@ -43,9 +48,19 @@ class Timer extends Component {
   }
 }
 
+Timer.propTypes = {
+  isAnswerButtonDisabled: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  isAnswerButtonDisabled: (state, type) => dispatch(newAction(
+    state, type,
+  )),
+});
+
 const mapStateToProps = (state) => ({
   email: state.main.email,
   playerName: state.main.playerName,
 });
 
-export default connect(mapStateToProps, null)(Timer);
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
--- src/reducers/questionsReducer.js
+++ src/reducers/questionsReducer.js
@@ -3,12 +3,18 @@ import { SAVE_QUESTIONS } from '../actions';
 const INITIAL_STATE = {
   questions: '',
   isQuestionsSaved: false,
+  isAnswerButtonDisabled: false,
 };
+
+export const CHANGE_BUTTON_STATE = 'CHANGE_BUTTON_STATE';
 
 function questions(state = INITIAL_STATE, action) {
   switch (action.type) {
   case SAVE_QUESTIONS:
     return { ...state, questions: action.token, isQuestionsSaved: true };
+  case CHANGE_BUTTON_STATE:
+    return { ...state, isAnswerButtonDisabled: action.state,
+    };
   default:
     return state;
   }
