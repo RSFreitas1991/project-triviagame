d9011788d09bc945011c9abfb246799f510b59c8
Marcos Danilo <marcosdanilo94@gmail.com>
2022-04-08 17:27:26 -0300

    Requisito 16 ok

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import GoToRanking from '../components/GoToRanking';
 import Header from '../components/header';
 
 class Feedback extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <div>
-        <Header />
-        <p data-testid="feedback-text">
-          Text de feedback
-        </p>
-      </div>
+      <>
+        <div>
+          <Header />
+          <p data-testid="feedback-text">
+            Text de feedback
+          </p>
+        </div>
+        <GoToRanking history={ history } />
+      </>
+
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape(PropTypes.any).isRequired,
+};
 
 export default Feedback;
