6cecbb1750e9b19f92a1a484afdce49f64f68808
Marcos Danilo <marcosdanilo94@gmail.com>
2022-04-08 15:19:42 -0300

    Iniciando requisito 15

---
+++ src/components/PlayAgain.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PlayAgain extends Component {
+  render() {
+    return (
+      <button type="button">Play Again</button>
+    );
+  }
+}
+
+export default PlayAgain;
