67262b2b64d792888ec78e9f0d94d3fc67524db7
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-11 20:06:17 -0300

    tentando resolver

--- src/components/GoHome.js
+++ src/components/GoHome.js
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { newAction, CLEAR_TOKEN } from '../actions';
 import { RESET_LOGIN } from '../reducers/main';
+import { RESET_QUESTIONS_SAVED } from '../reducers/questionsReducer';
 
 class GoHome extends Component {
   goHome = () => {
-    const { history, resetLogin, clearToken } = this.props;
+    const { history, resetLogin, clearToken, resetQuestionsSaved } = this.props;
     resetLogin();
     clearToken();
+    resetQuestionsSaved(false, RESET_QUESTIONS_SAVED);
     history.push('/');
   }
 
@@ -32,11 +34,13 @@ GoHome.propTypes = {
   }).isRequired,
   resetLogin: PropTypes.func.isRequired,
   clearToken: PropTypes.func.isRequired,
+  resetQuestionsSaved: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
   resetLogin: () => dispatch(newAction(null, RESET_LOGIN)),
   clearToken: () => dispatch(newAction('', CLEAR_TOKEN)),
+  resetQuestionsSaved: (state, type) => dispatch(newAction(state, type)),
 });
 
 export default connect(null, mapDispatchToProps)(GoHome);
