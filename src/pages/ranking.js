6dda05c3412c7a601b8025286a215c0635a1657b
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-11 16:39:46 -0300

    Estruturando o localStorage

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 import GoHome from '../components/GoHome';
 
 class Ranking extends Component {
+  componentDidMount() {
+    this.setLocalStorage();
+  }
+
+  setLocalStorage() {
+    const { playerName, score, email } = this.props;
+    const picture = this.gravatarCall(email);
+    const playersKey = [
+      { name: playerName, score, picture },
+    ];
+    const playersKeyString = JSON.stringify(playersKey);
+    localStorage.setItem('players', playersKeyString);
+  }
+
+  gravatarCall(email) {
+    const hash = md5(email).toString();
+    return (`https://www.gravatar.com/avatar/${hash}`);
+  }
+
   render() {
     const { history } = this.props;
     return (
@@ -18,6 +39,16 @@ class Ranking extends Component {
 
 Ranking.propTypes = {
   history: PropTypes.shape(PropTypes.any).isRequired,
+  playerName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
-export default Ranking;
+const mapStateToProps = (state) => ({
+  playerName: state.player.playerName,
+  email: state.player.email,
+  score: state.player.score,
+  token: state.token,
+});
+
+export default connect(mapStateToProps, null)(Ranking);
